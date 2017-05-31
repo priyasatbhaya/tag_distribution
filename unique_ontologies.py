0c4bf91d0e6b58b8012fb04d5a83b666ac09324d
priyasatbhaya <priyasatbhaya64@gmail.com>
2017-06-01 04:49:58 +0530

    for building file_dict.p

--- unique_ontologies.py
+++ unique_ontologies.py
@@ -6,10 +6,9 @@ import shutil
 file_from ='all_ontologies'
 file_to = 'ontology_set'
 
-def copy_files(file_dict,files):
+'''def copy_files(file_dict):
 	for key in file_dict.keys():
-		if(file_dict[key] in files):
-			shutil.copy2(os.path.join(file_from, file_dict[key]), os.path.join(file_to, file_dict[key]))
+			shutil.copy2(os.path.join(file_from, file_dict[key]), os.path.join(file_to, file_dict[key]))'''
 
 def parsing(filename,file_dict):
 	
@@ -26,17 +25,15 @@ def parsing(filename,file_dict):
 	   		file_dict[node['xml:base']]=filename
 
 def main():
-	files=[]
 	if os.path.isfile(os.path.join('pickle','file_dict.p')):
 		with open(os.path.join('pickle','file_dict.p'), 'rb') as f:
 			file_dict=pickle.load(f)
 	else : file_dict = {}
 
 	for filename in os.listdir(file_from):
-		files.append(filename)
 		parsing(filename,file_dict)
 
-	copy_files(file_dict,files)
+	#copy_files(file_dict,files)
 
 	print file_dict
 
