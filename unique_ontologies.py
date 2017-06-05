f6ebb10658945ce9c2a43db1983cdecc6a2d0501
priyasatbhaya <priyasatbhaya64@gmail.com>
2017-06-05 22:20:27 +0530

    parsing xml format also edit

--- unique_ontologies.py
+++ unique_ontologies.py
@@ -46,7 +46,7 @@ def parsing(filename,file_dict):
 	else :
 		node = tree.find('rdf:description',{'rdf:about':True})
 	if(node):
-		x=node['rdf:about'].split('#')[0]
+		x=node['rdf:about'].split('#')[0]+'#'
 		file_dict[x]=filename
 
 def main():
@@ -60,7 +60,7 @@ def main():
 	for filename in os.listdir(file_from):
 		parsing(filename,file_dict)
 
-	#print file_dict
+	print file_dict
 
 	#pickle 'file_dict' as 'file_dict.p' in 'pickle' directory 
 	with open(os.path.join('pickle','file_dict.p'), 'wb') as f:
